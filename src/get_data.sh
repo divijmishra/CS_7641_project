f9b4ea73b1cffdaa8db4172f21e44bed95e049ad
Divij Mishra <dmishra45@login-ice-2.pace.gatech.edu>
2024-04-06 22:31:44 -0400

    Modified get_data.sh to make data, model dirs

--- src/get_data.sh
+++ src/get_data.sh
@@ -3,6 +3,12 @@
 # Retrieve data as per https://github.com/rowanz/grover/tree/master/generation_examples
 # We choose to 
 
+mkdir data/raw/
+mkdir models/
+mkdir models/llm_fine-tuning
+mkdir models/text_topic_modeling
+mkdir models/title_topic_clustering
+
 wget https://storage.googleapis.com/grover-models/generation_examples/generator=base~dataset=p1.00.jsonl \
     -O data/raw/generator=base~dataset=p1.00.jsonl
     
