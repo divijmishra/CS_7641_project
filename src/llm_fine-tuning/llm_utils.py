302f1e937ef3228be2b1f1669fff5fae7e66dc79
Vamsi Varma Kalidindi <vkalidindi9@login-ice-3.pace.gatech.edu>
2024-04-06 23:30:36 -0400

    Updated llm_utils.py

--- src/llm_fine-tuning/llm_utils.py
+++ src/llm_fine-tuning/llm_utils.py
@@ -2,19 +2,34 @@
 
 
 
-def split_and_save_data(raw_data_path: str, save_data_path: str):
+def split_and_save_data(raw_data_path: str):
     """
     Takes in the path to the raw data as a .json file, 
     separates them based on whether "split" is train, val, test,
-    and saves each split as a separate json file in the specified path.
+    and saves each split as a separate json file in a new directory in raw_data_path
 
     Args:
         raw_data_path (str): Path to the raw data.
-        save_data_path (str): Directory where the splits will be saved.
 
     Returns:
         None
     """
+
+    save_dir_path = os.path.join(raw_data_path, 'split')
+    if os.path.exists(save_dir_path):
+        shutil.rmtree(save_dir_path)
+    os.makedirs(save_dir_path)
+
+    with open(raw_data_path, 'r', encoding='utf-8') as f:
+        for line in f:
+            data = json.loads(line)
+            split = data.get('split')
+            split_file_path = os.path.join(save_data_path, f"{split}.jsonl")
+            with open(split_file_path, 'a', encoding='utf-8') as split_file:
+                split_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+
+
+
     
     
 def load_data_as_lists(data_path: str) -> tuple:
@@ -30,3 +45,17 @@ def load_data_as_lists(data_path: str) -> tuple:
         texts: List[str] of article texts.
         labels: List[int] of article labels.
     """
+    texts = []
+    labels = []
+    with open(data_path, 'r', encoding='utf-8') as f:
+        for line in f:
+            data = json.loads(line)
+            texts.append(data['article'])
+            label = data.get('label')
+            if label == 'human':
+                labels.append(0)
+            elif label == 'machine':
+                labels.append(1)
+            else:
+                labels.append(-1)
+    return texts, labels
